10b56bc02095aa30820b7d3becf342dab4c2e9a4
michaeltetteh <miketetteh751@outlook.com>
2020-06-11 19:29:15 +0000

    added seed and store

--- calendar-app/src/App.vue
+++ calendar-app/src/App.vue
@@ -33,5 +33,17 @@ html, body {
   justify-content: center;
   -webkit-justify-content: center;
 }
+</style>
 
+<style lang="scss" scoped>
+// #app {
+//   height: inherit;
+//   background: #6e6e6e;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   -webkit-align-items: center;
+//   justify-content: center;
+//   -webkit-justify-content: center;
+// }
 </style>
