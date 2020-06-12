f447ba32133577af8dce6781859c390a14aacb32
michaeltetteh <miketetteh751@outlook.com>
2020-06-12 13:36:16 +0000

    emitted note and timestamp from child component to parent

---
+++ custom_events/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="notes-section">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <strong>Notes</strong>
+          <div v-for="note in notes" class="notes" :key="note">
+            {{ note }}
+          </div>
+        </div>
+        <div class="column has-text-centered">
+          <strong>Timestamp</strong>
+          <div v-for="timestamp in timestamps" class="timestamps" :key="timestamp">
+            {{ timestamp }}
+          </div>
+        </div>
+      </div>
+      <InputComponent 
+        :placeholder="placeholder"
+        @add-note="addNote" >
+      </InputComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputComponent from "./components/InputComponent.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    InputComponent
+  },
+
+  data () {
+    return {
+      notes: [],
+      timestamps: [],
+      placeholder: "Enter a note"
+    }
+  },
+
+  methods: {
+    addNote (event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ custom_events/src/components/InputComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input class="input is-small" type="text" 
+        :placeholder="placeholder"
+        v-model="input"
+        @keyup.enter="monitorEnterKey" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InputComponent",
+
+    props: ["placeholder"],
+
+    data () {
+        return {
+            input: ''
+        }
+    },
+
+    methods: {
+        monitorEnterKey () {
+            this.$emit('add-note',{
+                note: this.input,
+                timestamp: new Date().toLocaleString()
+            });
+            this.input = '';
+        }
+    }
+}
+</script>
