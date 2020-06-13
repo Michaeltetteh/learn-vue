3b9c08d9bcf62de4b7937f6b6ebb3be934d2f131
michaeltetteh <miketetteh751@outlook.com>
2020-06-13 21:11:29 +0000

    cart list component rendered

--- shopping-cart/src/store/module/cart/index.js
+++ shopping-cart/src/store/module/cart/index.js
@@ -1,10 +1,56 @@
-const state = {};
+import axios from 'axios';
 
-const mutations = {};
+const state = {
+    cartItem: []
+};
 
-const actions = {};
+const mutations = {
+    UPDATE_CART_ITEMS (state, payload) {
+        state.cartItem = payload
+    }
+};
 
-const getters = {};
+const actions = {
+    getCartItems ({ commit }) {
+        axios.get('/api/cart').then((res) => {
+            commit('UPDATE_CART_ITEMS',res.data)
+        });
+    },
+
+    addCartItem ({ commit }, cartItem) {
+        axios.post('/api/cart', cartItem).then((res) => {
+            commit("UPDATE_CART_ITEMS",res.data)
+        });
+    },
+
+    removeCartItem ({ commit }, cartItem) {
+        axios.post('/api/cart/delete',cartItem).then((res) => {
+            commit("UPDATE_CART_ITEMS", res.data)
+        });
+    },
+
+    removeAllCartItems ({ commit }) {
+        axios.post('/api/cart/delete/all').then((res) => {
+            commit("UPDATE_CART_ITEMS", res.data)
+        });
+    }
+};
+
+const getters = {
+    cartItems: state => state.cartItem,
+    
+    cartTotal: state => {
+        return state.cartItem.reduce((acc, cartItem) => {
+            return ( cartItem.quantity * cartItem.price) + acc;
+        }, 0).toFixed(2);
+    },
+
+    cartQuantity: state => {
+        return state.cartItem.reduce((acc, cartItem) => {
+            return cartItem.quantity + acc;
+        }, 0);
+    }
+};
 
 const cartModule = {
     state,
