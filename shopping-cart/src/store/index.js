77b3e520f1c62e9889430497012c04c09f2293b4
michaeltetteh <miketetteh751@outlook.com>
2020-06-13 16:45:06 +0000

    modularization of vuex store

--- shopping-cart/src/store/index.js
+++ shopping-cart/src/store/index.js
@@ -1,18 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import state from './state.js'
-import mutations from './mutations.js'
-import actions from './actions.js'
-import getters from './getters.js'
+import cart from './module/cart'
+import product from './module/product'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state,
-  mutations,
-  actions,
-  getters,
   modules:{
-
+    cart,
+    product
   }
 })
