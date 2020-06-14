6d2a3dcb0f6b0348e536aeb04c60cb1d05f39534
michaeltetteh <miketetteh751@outlook.com>
2020-06-14 18:11:32 +0000

    btn click evt listeners

--- form-handling/app/main.js
+++ form-handling/app/main.js
@@ -1,3 +1,33 @@
-new VTTCue({
-    el: "#app"
+const ButtonRow = {
+    template:
+    `
+    <div>
+        <button @click="onHoodieClick" class="ui button">Hoodie</button>
+        <button @click="onTeeClick" class="ui button">Tee</button>
+        <button @click="onFittedCapClick" class="ui button">Fitted Cap</button>
+        <button @click="onJacketClick" class="ui button">Jacket</button>
+    </div>
+    `,
+    methods: {
+        onHoodieClick(evt) {
+            console.log("hoodie btn clicked",evt);
+        },
+        onTeeClick(evt) {
+            console.log("Tee btn clicked",evt);
+        },
+        onFittedCapClick(evt) {
+            console.log("Fitted Cap btn clicked", evt);
+        },
+        onJacketClick(evt) {
+            console.log("Jacket btn clicked", evt);
+        }
+    }
+}
+
+
+new Vue({
+    el: "#app",
+    components: {
+        'button-row': ButtonRow 
+    }
 })
