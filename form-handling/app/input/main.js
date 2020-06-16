a325a4520ce5a710893d3aca43d0982e0db6fd77
michaeltetteh <miketetteh751@outlook.com>
2020-06-16 10:41:46 +0000

    update to form input

--- form-handling/app/input/main.js
+++ form-handling/app/input/main.js
@@ -1,26 +1,34 @@
 const InputForm = {
-    template: 
-    `
-    <div class="input-form" >
-        <form  class="ui form" >
-            <div class="field" >
-                <input ref="newItem" type = "text" placeholder = "Add an item!" >
+    template: `
+    <div class="input-form">
+        <form @submit="submitForm" class="ui form">
+            <div class="field">
+                <input v-model="newInput" type="text" placeholder="Add an item!">
             </div> 
             <button class="ui button">Submit</button>
          </form> 
     </div>
-    `
+    `,
+
+    data() {
+        return {
+            newInput: ''
+        }
+    },
+    
+    methods: {
+        submitForm(evt) {
+            evt.preventDefault();
+            console.log(this.newInput)
+        }
+    }
+    
 }
 
 new Vue({
     el: "#app",
     components: {
         'input-form': InputForm
-    },
-    methods: {
-        submitForm(evt) {
-            evt.preventDefault();
-            console.log(this.$ref.newItem.value)
-        }
     }
 })
+
