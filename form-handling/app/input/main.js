32db5d55351a9d035694b433492ba71dec470b10
michaeltetteh <miketetteh751@outlook.com>
2020-07-22 12:42:21 +0000

    form field validation

--- form-handling/app/input/main.js
+++ form-handling/app/input/main.js
@@ -3,10 +3,14 @@ const InputForm = {
     <div class="input-form">
         <form @submit="submitForm" class="ui form">
             <div class="field">
-                <input v-model="fields.newInput" type="text" placeholder="Add an item!">
+                <label>New Item</label>
+                <input v-model="fields.newItem" type="text" placeholder="Add an item!">
+                <span style="color: red;"> {{ fieldErrors.newItem }} </span>
             </div> 
             <div class="field">
+                <label>Email</label>
                 <input v-model="fields.email" type="email" placeholder="example@example.com">
+                <span style="color: red;"> {{ fieldErrors.email }} </span>
             </div>
             <div class="field">
                 <label>Urgency</label>
@@ -16,12 +20,14 @@ const InputForm = {
                     <option>Moderate</option>
                     <option>Urgent</option>
                 </select>
+                <span style="color: red;"> {{ fieldErrors.urgency }} </span>
             </div>
             <div class="field">
                 <div class="ui checkbox">
                     <input v-model="fields.termsAndConditions" type="checkbox" />
                     <label>I accept the terms and conditions</label>
                 </div>
+                <span style="color: red;"> {{ fieldErrors.termsAndConditions }} </span>
             </div>
             <button class="ui button">Submit</button>
          </form> 
@@ -37,10 +43,16 @@ const InputForm = {
     data() {
         return {
             fields: {
-                newInput: '',
+                newItem: '',
                 email: '',
                 urgency: '',
                 termsAndConditions: false
+            },
+            fieldErrors:{
+                newItem: undefined,
+                email: undefined,
+                urgency: undefined,
+                termsAndConditions: undefined
             },
             items: []
         }
@@ -48,10 +60,39 @@ const InputForm = {
     
     methods: {
         submitForm(evt) {
-            this.items.push(this.fields.newInput);
-            console.log(this.fields.newInput)
-            this.fields.newInput = "";
             evt.preventDefault();
+
+            this.fieldErrors = this.validateForm(this.fields);
+            // console.log(this.fieldErrors);
+            // console.log(this.validateForm(this.fields));
+            if (Object.keys(this.fieldErrors).length) return;
+
+            this.items.push(this.fields.newItem);
+            this.fields.newItem = ''
+            this.fields.email = ''
+            this.fields.urgency = ''
+            this.fields.termsAndConditions = false
+        },
+
+        validateForm(fields) {
+            const errors = {};
+            if (!fields.newItem) errors.newItem = "New Item Required";
+            if (!fields.email) errors.email = "Email Required";
+            if (!fields.urgency) errors.urgency = "Urgency Required";
+            if (!fields.termsAndConditions) {
+                errors.termsAndConditions = "Terms and conditions have to be approved";
+            }
+
+            if (fields.email && !this.isEmail(fields.email)) {
+                errors.email = "Invalid Email";
+            }
+
+            return errors;
+        },
+
+        isEmail(email) {
+            const re = /\S+@\S+\.\S+/;
+            return re.test(email);
         }
     }
     
