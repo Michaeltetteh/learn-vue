316caa06a954cc44bfe27d6f830587781db2b815
michaeltetteh <miketetteh751@outlook.com>
2020-07-23 12:09:23 +0000

    integrating vuex

--- form-handling/app/input/main.js
+++ form-handling/app/input/main.js
@@ -67,19 +67,12 @@ const InputForm = {
 
     data() {
         return {
-            fields: {
-                newItem: '',
-                email: '',
-                urgency: '',
-                termsAndConditions: false
-            },
             fieldErrors:{
                 newItem: undefined,
                 email: undefined,
                 urgency: undefined,
                 termsAndConditions: undefined
             },
-            items: [],
             loading: false,
             saveStatus: "READY"
         }
@@ -154,35 +147,36 @@ const InputForm = {
     },
 }
 
-let apiClient = {
-    loadItems: function () {
-        return {
-            then: function (cb) {
-                setTimeout(() => {
-                    cb(JSON.parse(localStorage.items || '[]'));
-                },1000);
-            },
-        };
-    },
+// let apiClient = {
+//     loadItems: function () {
+//         return {
+//             then: function (cb) {
+//                 setTimeout(() => {
+//                     cb(JSON.parse(localStorage.items || '[]'));
+//                 },1000);
+//             },
+//         };
+//     },
 
-    saveItems: function (items) {
-        const success = !!(this.count++ % 2);
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (!success) return reject({success});
-                localStorage.items = JSON.stringify(items);
-                return resolve({success});
-            }, 1000);
-        });
-    },
+//     saveItems: function (items) {
+//         const success = !!(this.count++ % 2);
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (!success) return reject({success});
+//                 localStorage.items = JSON.stringify(items);
+//                 return resolve({success});
+//             }, 1000);
+//         });
+//     },
 
-    count: 1,
-}
+//     count: 1,
+// }
 
 new Vue({
     el: "#app",
+    store,
     components: {
         'input-form': InputForm
-    }
+    },
 })
 
